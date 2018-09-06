e0035ab28d2972e1e9bfa394c92b6fad18ba4413
HuangZonghao <coding@hzh.io>
2018-09-06 11:30:17 -0400

    Remove trailing whitespaces

--- cpp/include/strct.h
+++ cpp/include/strct.h
@@ -1,7 +1,7 @@
 /*
  * GPisMap - Online Continuous Mapping using Gaussian Process Implicit Surfaces
  * https://github.com/leebhoram/GPisMap
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License v3 as published by
  * the Free Software Foundation.
