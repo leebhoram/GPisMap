d0223c381385aee531cc2e573bfb06c86f9d58a9
HuangZonghao <coding@hzh.io>
2018-09-06 01:56:40 -0400

    Add license information

--- cpp/include/ObsGP.h
+++ cpp/include/ObsGP.h
@@ -1,7 +1,22 @@
-////////////////////////////////////////////////////
-// Observation GP Module
-//
-// Bhoram Lee (bhoram.lee@gmail.com)
+/*
+ * GPisMap - Online Continuous Mapping using Gaussian Process Implicit Surfaces
+ * https://github.com/leebhoram/GPisMap
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of any FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License v3 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3
+ * along with this program; if not, you can access it online at
+ * http://www.gnu.org/licenses/gpl-3.0.html.
+ *
+ * Authors: Bhoram Lee <bhoram.lee@gmail.com>
+ *          Huang Zonghao<ac@hzh.io>
+ */
 
 #ifndef __ObsGP_H__
 #define __ObsGP_H__
--- cpp/include/quadtree.h
+++ cpp/include/quadtree.h
@@ -1,3 +1,22 @@
+/*
+ * GPisMap - Online Continuous Mapping using Gaussian Process Implicit Surfaces
+ * https://github.com/leebhoram/GPisMap
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of any FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License v3 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3
+ * along with this program; if not, you can access it online at
+ * http://www.gnu.org/licenses/gpl-3.0.html.
+ *
+ * Authors: Bhoram Lee <bhoram.lee@gmail.com>
+ */
+
 #ifndef __QUADTREE_H_
 #define __QUADTREE_H_
 
